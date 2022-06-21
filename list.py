1b0bd626335e53c5f4f0c3ccf406b26c4288f2c9
Wanda-Elizabeth <wandaelizabeth@gmail.com>
2022-06-21 14:36:44 +0300

    list

---
+++ list.py
@@ -0,0 +1,61 @@
+def Numbers(a):
+    a.remove(a[-2])
+    print(a)
+a=[2,4,6,8]   
+Numbers(a)
+
+def week(days):
+    print(days.count("Monday"))
+days=["Monday","Tuesday","Friday","Monday"]
+week(days) 
+
+def number(list):
+    min.list[0]
+    for x in list:
+        print()
+        number (list)
+     
+def list(b):
+    if 4 in b:
+        return True
+    else:
+      return False
+b=[1,2,3,4,5]
+list(b)
+
+def divisible_by_seven():
+    for x in range (100,200):
+        if x%7==0:
+            print(x)
+        else:
+            print("Not divisible by seven")    
+divisible_by_seven()
+  
+def greens(b):
+    b.pop()
+    print(b)
+b=["apple","grapes","pineapple"]  
+greens(b)
+
+def greens(b):
+    b.remove("pineapple")
+    print(b)
+b=["apple","grapes","pineapple"]  
+greens(b)
+cars=["Ford","BMW","Volvo"]
+cars.sort()
+print(cars)
+
+def number_in():
+    num1=int(input("Enter Number"))
+    num2=int(input("Enter Number"))
+    sum=num1+num2
+    if sum > 10: 
+     print("Greater than ten")
+    elif sum < 10:
+        print("Less than ten")
+    elif sum== 10:
+        print("Equal to ten")  
+number_in()         
+
+
